0a24e4bd2d5e0e8f4c31a94f0b0899cd497d3f1c
Madhav Sahi <madhavsahi16@gmail.com>
2025-07-16 23:30:29 +0530

    initialised components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">
+      {/* <h1 className="text-4xl font-bold text-blue-600">
         Tailwind is working! 🎉
-      </h1>
+      </h1> */}
+      <Header />
     </div>
   );
 }
 
 export default App;
+
+// Header
+// Body
+// - Sidebar
+//  -- Side items
+// - Main Container
+//  -- Button Categories
+//  -- Video Container
+//    --- Video Card    
